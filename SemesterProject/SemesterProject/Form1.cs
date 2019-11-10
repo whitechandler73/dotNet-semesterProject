55f8e05d9a937cb2eebd68dfe603b79bb16c2fb1
Michael White <whitechandler73@gmail.com>
2019-11-09 23:13:04 -0600

    Changed size of Form1 window - Initial commit on branch

--- SemesterProject/SemesterProject/Form1.cs
+++ SemesterProject/SemesterProject/Form1.cs
@@ -16,5 +16,10 @@ namespace SemesterProject
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
